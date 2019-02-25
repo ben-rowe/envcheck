3ef48842bb8fa851b06317e4fbe6182f7c56d48d
ben-rowe <you.kissmyarse@gmail.com>
2019-02-24 23:51:45 -0800

    Update envcheck.go
    addition of fatal switch

--- envcheck.go
+++ envcheck.go
@@ -5,12 +5,20 @@ import (
 	"os"
 )
 
-func GetEnv(key, fallback string) string {
+func GetEnv(key, fallback string, fatal bool) string {
 	// Return the correct enviroment variable if found
 	if value, ok := os.LookupEnv(key); ok {
 		return value
 	}
+	
+	// Do we want to log or kill the program?
+	if fatal {
+		// If the environment variable is not found exit the program.
+		log.Fatal("Fatal. Enviroment variable '%s' was not found. Exiting.", key)
+	}
+	
 	// If the environment variable is not found utilise the default and throw a warning
 	log.Printf("Warning. Enviroment variable '%s' was not found. Falling back to the default value '%s'", key, fallback)
+	
 	return fallback
 }
